ea610a83d651f4fb0c62aa5e66c86fb3b9719098
TakeshiShiomi <takeyan403@gmail.com>
2022-03-29 13:38:51 +0900

    google map導入,ログアウト時のhome画面nilエラー修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,12 @@
 class HomesController < ApplicationController
+
   def top
-    @feeds = Photo.where(user_id: [current_user.id, *current_user.following_ids]).order(created_at: :desc)
+    if user_signed_in?
+      @feeds = Photo.where(user_id: [current_user.id, *current_user.following_ids]).order(created_at: :desc)
+    else
+    end
+  end
+
+  def about
   end
 end
