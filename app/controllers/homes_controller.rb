4277c363fd75d3276a86899074bb6f67c094b314
TakeshiShiomi <takeyan403@gmail.com>
2022-03-29 16:57:49 +0900

    rubocop導入

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,4 @@
 class HomesController < ApplicationController
-
   def top
     if user_signed_in?
       @feeds = Photo.where(user_id: [current_user.id, *current_user.following_ids]).order(created_at: :desc)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(params.require(:user).permit(:name, :email, :image))
