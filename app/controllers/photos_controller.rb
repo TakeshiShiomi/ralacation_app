4a62975662ab6716ab13450e7296b31d5bce3979
TakeshiShiomi <takeyan403@gmail.com>
2022-04-01 15:42:24 +0900

    ログインなしでユーザーマイページ及び登録地点のサイトへ飛ぶように修正

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
 
   def new
     @photo = current_user.photos.new
