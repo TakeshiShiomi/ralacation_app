e5ec65f6e56ae851b4423ff2b1241bd45a523b9e
TakeshiShiomi <takeyan403@gmail.com>
2022-04-06 05:55:15 +0900

    リロードしないとページ遷移しない問題を解消

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+//import Turbolinks from "turbolinks" 削除することでリロードしないとページ遷移しない問題を対処できる
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
