7719653bd870358430749429d2fbfe8b36d0547e
TakeshiShiomi <takeyan403@gmail.com>
2022-04-05 10:26:39 +0900

    Herokuの画面挙動修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-import "../stylesheets/application"
+import "../stylesheets/application.scss";
 
 var jQuery = require('jquery')
 global.$ = global.jQuery = jQuery;
